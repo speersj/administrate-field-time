2c8b76dfbd7f638eec93201ac327d05f3bf9b20d
Justin Speers <speers.justin@gmail.com>
2017-09-18 20:11:59 -0700

    wait for dom load to set time picker inputs

--- app/assets/javascripts/administrate/components/date_time_picker.js
+++ app/assets/javascripts/administrate/components/date_time_picker.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function () {
   $(".timepicker").datetimepicker({debug: false, format: "HH:mm:ss"});
   $(".datetimepicker").datetimepicker({debug: false, format: "YYYY-MM-DD HH:mm:ss"});
 });
